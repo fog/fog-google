e7cd5574345ea0a1b8849c742097a98c6497772d
Isaac Hollander McCreery <ihmccreery@google.com>
2015-04-10 00:21:55 +0000

    [shindo-minitest] make minitest_helper use :test for Fog.credential, update CONTRIBUTING to reflect it

--- spec/minitest_helper.rb
+++ spec/minitest_helper.rb
@@ -13,3 +13,7 @@ end
 require File.join(File.dirname(__FILE__), '../lib/fog/google.rb')
 
 Coveralls.wear! if ENV['COVERAGE']
+
+# Use :test credentials in ~/.fog for live integration testing
+# XXX not sure if this will work on Travis CI or not
+Fog.credential = :test
