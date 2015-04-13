370c1a18de57a4c7a57c1705a541a54194275d01
Isaac Hollander McCreery <ihmccreery@google.com>
2015-04-13 02:50:13 +0000

    [shindo-minitest] refactor CollectionSpec to wait for a resource to be destroyed before continuing

--- spec/helpers/collection_spec.rb
+++ spec/helpers/collection_spec.rb
@@ -1,8 +1,12 @@
 module Fog::CollectionSpec
   extend Minitest::Spec::DSL
 
+  # this should be reimplemented in the subject's spec if the subject has required params
+  def params
+    {}
+  end
+
   it "can do ::new, #save, ::create, #all, #identity, ::get, and #destroy" do
-    puts subject
     instance_one = subject.new(params)
     instance_one.save
     instance_two = subject.create(params)
@@ -17,6 +21,9 @@ module Fog::CollectionSpec
 
     instance_one.destroy
     instance_two.destroy
+
+    Fog.wait_for { !subject.all.map(&:identity).include? instance_one.identity }
+    Fog.wait_for { !subject.all.map(&:identity).include? instance_two.identity }
 
     assert_nil subject.get(subject.new(params).identity)
   end
