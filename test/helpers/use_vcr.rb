da81454265b15513b2565cb271062f055b1e4969
Isaac Hollander McCreery <ihmccreery@google.com>
2015-04-15 21:23:43 +0000

    [shindo-minitest] VCR uses different wait intervals for recording and playback

--- test/helpers/use_vcr.rb
+++ test/helpers/use_vcr.rb
@@ -1,10 +1,16 @@
 module UseVCR
+  VCR_RECORDING_INTERVAL = 10
+  VCR_PLAYBACK_INTERVAL = 0
+
   def before_setup
     super
     VCR.insert_cassette("#{self.class.to_s}_#{name}")
+    @default_fog_interval = Fog.interval
+    Fog.interval = VCR.current_cassette.recording? ? VCR_RECORDING_INTERVAL : VCR_PLAYBACK_INTERVAL
   end
 
   def after_teardown
+    Fog.interval = @default_fog_interval
     VCR.eject_cassette
     super
   end
