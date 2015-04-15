6f967f9eaaaa5661437ac7a7cf2629156d576414
Isaac Hollander McCreery <ihmccreery@google.com>
2015-04-15 17:13:35 +0000

    [shindo-minitest] skip TestServer::test_bootstrap_ssh_destroy when mocking with VCR

--- test/fog/google/models/compute/test_server.rb
+++ test/fog/google/models/compute/test_server.rb
@@ -22,19 +22,20 @@ class TestServer < MiniTest::Test
               :disks => [test_disk]}
   end
 
-  # XXX this test is currently broken with VCR
-  # we need to figure out a way to get ssh working with VCR, or work around the need to ssh.  The problem is with this line:
-  #     https://github.com/fog/fog-core/blob/master/lib/fog/compute/models/server.rb#L93
-  # where it returns false because the ssh times out,
-
-  # TODO or maybe because of some other error!?
-
-  # def test_bootstrap_ssh_destroy
-  #   instance = @subject.bootstrap
-
-  #   assert instance.ready?
-  #   instance.wait_for { sshable? }
-  #   assert_match /Linux/, instance.ssh("uname").first.stdout
-  #   assert_equal instance.destroy.operation_type, "delete"
-  # end
+  def test_bootstrap_ssh_destroy
+    # XXX this test is currently broken with VCR
+    # we need to figure out a way to get ssh working with VCR, or work around the need to ssh.  The problem is with this line:
+    #     https://github.com/fog/fog-core/blob/master/lib/fog/compute/models/server.rb#L93
+    # where it returns false because the ssh times out,
+    # TODO or maybe because of some other error!?
+    if VCR.current_cassette.recording?
+      instance = @subject.bootstrap
+      assert instance.ready?
+      instance.wait_for { sshable? }
+      assert_match /Linux/, instance.ssh("uname").first.stdout
+      assert_equal instance.destroy.operation_type, "delete"
+    else
+      skip("this test is currently broken with VCR")
+    end
+  end
 end
