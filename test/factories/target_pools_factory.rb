4ed5c14075032342fb91a0c4bec3db1fdc9c5cab
Isaac Hollander McCreery <ihmccreery@google.com>
2015-04-16 22:11:49 +0000

    [shindo-minitest] convert TargetPool tests to minitest

---
+++ test/factories/target_pools_factory.rb
@@ -0,0 +1,25 @@
+require "factories/collection_factory"
+require "factories/servers_factory"
+require "factories/http_health_checks_factory"
+
+class TargetPoolsFactory < CollectionFactory
+  def initialize
+    @subject = Fog::Compute[:google].target_pools
+    @http_health_checks = HttpHealthChecksFactory.new
+    @servers = ServersFactory.new
+    super
+  end
+
+  def cleanup
+    super
+    @servers.cleanup
+    @http_health_checks.cleanup
+  end
+
+  def params
+    params = {:name => create_test_name,
+              :region => TEST_REGION,
+              :instances => [@servers.create.self_link],
+              :healthChecks => [@http_health_checks.create.self_link]}
+  end
+end
