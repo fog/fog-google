fe448a8db9c29c267c1186608d9b6f1a8afdf51d
Isaac Hollander McCreery <ihmccreery@google.com>
2015-04-21 22:23:36 +0000

    Fix image test to reflect API requirements: reference an actual image on Google Cloud Storage; related to, but does not close, #22

--- tests/models/compute/image_tests.rb
+++ tests/models/compute/image_tests.rb
@@ -1,6 +1,6 @@
 require 'securerandom'
 Shindo.tests("Fog::Compute[:google] | image model", ['google']) do
   random_string = SecureRandom.hex
-  source = 'https://www.google.com/images/srpr/logo4w.png'
+  source = 'http://storage.googleapis.com/fog-test-bucket/fog-test-raw-disk-source.image.tar.gz'
   model_tests(Fog::Compute[:google].images, {:name => "fog-test-images-#{random_string}", "rawDisk" => { "source" => source } })
 end
