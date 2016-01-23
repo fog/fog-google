f7a974bef942f1a8614c90e6447346cfacecd030
Dean Putney <dean@glowforge.com>
2016-01-22 16:41:14 -0800

    rubocop -a

--- lib/fog/google/requests/storage_json/get_bucket.rb
+++ lib/fog/google/requests/storage_json/get_bucket.rb
@@ -9,10 +9,10 @@ module Fog
         # ==== Parameters
         # * bucket_name<~String> - name of bucket to list object keys from
         # * options<~Hash> - config arguments for list.  Defaults to {}.
-        #   * 'ifMetagenerationMatch'<~Long> - Makes the return of the bucket metadata 
-        #     conditional on whether the bucket's current metageneration matches the 
+        #   * 'ifMetagenerationMatch'<~Long> - Makes the return of the bucket metadata
+        #     conditional on whether the bucket's current metageneration matches the
         #     given value.
-        #   * 'ifMetagenerationNotMatch'<~Long> - Makes the return of the bucket 
+        #   * 'ifMetagenerationNotMatch'<~Long> - Makes the return of the bucket
         #     metadata conditional on whether the bucket's current metageneration does
         #     not match the given value.
         #   * 'projection'<~String> - Set of properties to return. Defaults to 'noAcl',
@@ -24,7 +24,7 @@ module Fog
         #     See Google documentation on Bucket resource:
         #     https://cloud.google.com/storage/docs/json_api/v1/buckets#resource
         #
-        
+
         def get_bucket(bucket_name, options = {})
           raise ArgumentError.new("bucket_name is required") unless bucket_name
 
