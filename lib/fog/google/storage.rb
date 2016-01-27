08c2562bafd9012727988ec1b92eb0574da06d9d
Dean Putney <dean@glowforge.com>
2016-01-26 16:34:31 -0800

    Allow options for Fog::Storage::Google to be optional, switch between StorageJSON and StorageXML with Fog.credentials as well

--- lib/fog/google/storage.rb
+++ lib/fog/google/storage.rb
@@ -4,8 +4,9 @@ require "fog/google/storage_json"
 module Fog
   module Storage
     class Google < Fog::Service
-      def self.new(options)
-        if options.keys.join(" ").include? "key"
+      def self.new(options = {})
+        if options.keys.join(" ").include? "client_email" or
+          Fog.credentials.keys.join(" ").include? "client_email"
           Fog::Storage::GoogleJSON.new(options)
         else
           Fog::Storage::GoogleXML.new(options)
