7b5b027bfe6eb05f7d8182e48231e3b1f18418ad
Dean Putney <dean@glowforge.com>
2016-01-28 11:00:41 -0800

    Handle case where Fog.credentials is empty

--- lib/fog/google/storage.rb
+++ lib/fog/google/storage.rb
@@ -5,8 +5,14 @@ module Fog
   module Storage
     class Google < Fog::Service
       def self.new(options = {})
+        begin
+          fog_creds = Fog.credentials
+        rescue
+          fog_creds = nil
+        end
+
         if options.keys.include? :google_storage_access_key_id or
-          (Fog.credentials and Fog.credentials.keys.include? :google_storage_access_key_id)
+          (not fog_creds.nil? and fog_creds.keys.include? :google_storage_access_key_id)
           Fog::Storage::GoogleXML.new(options)
         else
           Fog::Storage::GoogleJSON.new(options)
