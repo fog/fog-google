482f7eec8efb3d7d1d134d2f3673f5feebfb54fe
Eric Johnson <erjohnso@google.com>
2016-03-16 13:14:31 +0000

    fix examples to be consistent with others

--- examples/monitoring/timeseries_collection.rb
+++ examples/monitoring/timeseries_collection.rb
@@ -1,4 +1,10 @@
-require "fog"
+# All examples presume that you have a ~/.fog credentials file set up.
+# # More info on it can be found here: http://fog.io/about/getting_started.html
+#
+require "bundler"
+Bundler.require(:default, :development)
+# Uncomment this if you want to make real requests to GCE (you _will_ be billed!)
+# WebMock.disable!
 
 def test
   connection = Fog::Google::Monitoring.new
