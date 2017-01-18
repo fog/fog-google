378be312333d4d50dd1ed44ce16f28886022cd82
Artem <temikus@google.com>
2017-01-18 16:42:00 +1100

    Fix precreated_client example

--- examples/precreated_client.rb
+++ examples/precreated_client.rb
@@ -1,3 +1,14 @@
+# All examples presume that you have a ~/.fog credentials file set up.
+# More info on it can be found here: http://fog.io/about/getting_started.html
+
+require "bundler"
+Bundler.require(:default, :development)
+# Uncomment this if you want to make real requests to GCE (you _will_ be billed!)
+# WebMock.disable!
+
+# This example shows how to work with fog using a pre-created Google API client
+# with specific parameters, should you want to for any reason.
+
 def test
   client = Google::APIClient.new(:application_name => "supress")
   connection = Fog::Compute.new(:provider => "Google", :google_client => client)
@@ -9,3 +20,5 @@ def test
     p e.message
   end
 end
+
+test
