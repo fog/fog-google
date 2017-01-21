1f5d9b1c23f12197a58c18a05bc84f79681be0da
Artem Yakimenko <temikus@google.com>
2017-01-21 12:44:46 +1100

    Remove redundant step from bootstrap example
    Bootstrap already checks for sshable?

--- examples/bootstrap.rb
+++ examples/bootstrap.rb
@@ -10,7 +10,6 @@ def test
   connection = Fog::Compute.new(:provider => "Google")
 
   server = connection.servers.bootstrap
-  server.wait_for { sshable? }
 
   raise "Could not bootstrap sshable server." unless server.ssh("whoami")
   raise "Could not delete server." unless server.destroy
