94ae88f8cf86bd493389c07d6224f05bf274d2d2
Artem Yakimenko <temikus@google.com>
2017-01-21 12:46:55 +1100

    Change test for proper state method

--- test/integration/compute/test_compute_addresses_collection.rb
+++ test/integration/compute/test_compute_addresses_collection.rb
@@ -50,7 +50,7 @@ class TestComputeAddressesCollection < FogIntegrationTest
       ],
       :external_ip => my_address.address
     )
-    my_server.wait_for { my_server.state != "PROVISIONING" }
+    my_server.wait_for { provisioning? }
 
     # And verify that it's correctly assigned
     assert_equal(
