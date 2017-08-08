a08a9c1077763826e5623f7317f9f85bb53162b4
Dawid Janczak <dawid.janczak@gmail.com>
2017-08-08 10:37:00 +0800

    Build cloud monitoring service

--- lib/fog/google/monitoring/real.rb
+++ lib/fog/google/monitoring/real.rb
@@ -4,15 +4,14 @@ module Fog
       class Real
         include Fog::Google::Shared
 
-        attr_accessor :client
         attr_reader :monitoring
 
         def initialize(options)
           shared_initialize(options[:google_project], GOOGLE_MONITORING_API_VERSION, GOOGLE_MONITORING_BASE_URL)
           options[:google_api_scope_url] = GOOGLE_MONITORING_API_SCOPE_URLS.join(" ")
 
-          @client = initialize_google_client(options)
-          @monitoring = @client.discovered_api("cloudmonitoring", api_version)
+          initialize_google_client(options)
+          @monitoring = ::Google::Apis::CloudMonitoringV2beta2::CloudMonitoringService.new
         end
       end
     end
