b9d81cc633ebfd8f402a87bb2e6314ba01399c86
Emily Ye <emilyye@google.com>
2017-11-28 00:35:52 +0000

    add filters to list disk requests

--- lib/fog/compute/google/requests/list_aggregated_disks.rb
+++ lib/fog/compute/google/requests/list_aggregated_disks.rb
@@ -13,8 +13,16 @@ module Fog
         #
         # @param options [Hash] Optional hash of options
         # @option options [String] :filter Filter expression for filtering listed resources
-        def list_aggregated_disks(options = {})
-          @compute.list_aggregated_disk(@project, :filter => options[:filter])
+        # @option options [String] :max_results
+        # @option options [String] :order_by
+        # @option options [String] :page_token
+        def list_aggregated_disks(filter: nil, max_results: nil,
+                                  order_by: nil, page_token: nil)
+          @compute.list_aggregated_disks(
+            @project,
+            :filter => filter, :max_results => max_results,
+            :order_by => order_by, :page_token => page_token
+          )
         end
       end
     end
--- lib/fog/compute/google/requests/list_disks.rb
+++ lib/fog/compute/google/requests/list_disks.rb
@@ -2,7 +2,7 @@ module Fog
   module Compute
     class Google
       class Mock
-        def list_disks(_zone_name)
+        def list_disks(_zone_name, _opts = {})
           Fog::Mock.not_implemented
         end
       end
@@ -12,8 +12,13 @@ module Fog
         # https://cloud.google.com/compute/docs/reference/latest/disks/list
         #
         # @param zone_name [String] Zone to list disks from
-        def list_disks(zone_name)
-          @compute.list_disks(@project, zone_name)
+        def list_disks(zone_name, filter: nil, max_results: nil,
+                       order_by: nil, page_token: nil)
+          @compute.list_disks(
+            @project, zone_name,
+            :filter => filter, :max_results => max_results,
+            :order_by => order_by, :page_token => page_token
+          )
         end
       end
     end
