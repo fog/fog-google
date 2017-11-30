50091dc5baa4338428e16f2329f30893b309c18c
Emily Ye <emilyye@google.com>
2017-11-30 20:00:54 +0000

    upgrade regional forwarding rules

--- lib/fog/compute/google/requests/list_aggregated_forwarding_rules.rb
+++ lib/fog/compute/google/requests/list_aggregated_forwarding_rules.rb
@@ -2,17 +2,16 @@ module Fog
   module Compute
     class Google
       class Mock
-        def list_forwarding_rules(_region, _opts = {})
+        def list_aggregated_forwarding_rules(_opts = {})
           Fog::Mock.not_implemented
         end
       end
 
       class Real
-        def list_forwarding_rules(region,
-                                  filter: nil, max_results: nil,
-                                  order_by: nil, page_token: nil)
-          @compute.list_forwarding_rules(
-            @project, region,
+        def list_aggregated_forwarding_rules(filter: nil, max_results: nil,
+                                             order_by: nil, page_token: nil)
+          @compute.list_aggregated_forwarding_rules(
+            @project,
             :filter => filter, :max_results => max_results,
             :order_by => order_by, :page_token => page_token
           )
