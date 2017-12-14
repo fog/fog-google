009f658a50858e7c0505624d7631842569a275f2
Emily Ye <emilyye@google.com>
2017-12-14 21:42:45 +0000

    fix list_address filters

--- lib/fog/compute/google/requests/list_addresses.rb
+++ lib/fog/compute/google/requests/list_addresses.rb
@@ -2,18 +2,23 @@ module Fog
   module Compute
     class Google
       class Mock
-        def list_addresses(_region_name)
+        def list_addresses(_region_name, _opts = {})
           Fog::Mock.not_implemented
         end
       end
 
       class Real
         # List address resources in the specified project
-        # https://cloud.google.com/compute/docs/reference/latest/addresses/list
-        #
-        # @param region_name [String] Region for address
-        def list_addresses(region_name)
-          @compute.list_addresses(@project, region_name)
+        # @see https://cloud.google.com/compute/docs/reference/latest/addresses/list
+        def list_addresses(region_name, filter: nil, max_results: nil,
+                           order_by: nil, page_token: nil)
+          @compute.list_addresses(
+            @project, region_name,
+            :filter => filter,
+            :max_results => max_results,
+            :order_by => order_by,
+            :page_token => page_token
+          )
         end
       end
     end
