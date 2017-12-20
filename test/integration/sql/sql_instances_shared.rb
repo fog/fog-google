78234be7e583af81b1a75900e3c75820a185b549
Emily Ye <emilyye@google.com>
2017-12-20 19:21:01 +0000

    ignore errors unrelated to 404s only in teardown for sql

--- test/integration/sql/sql_instances_shared.rb
+++ test/integration/sql/sql_instances_shared.rb
@@ -48,7 +48,8 @@ class TestSqlInstancesShared < FogIntegrationTest
   def try_delete_instance(instance)
     @client = Fog::Google::SQL.new
     wait_until_complete { @client.delete_instance(instance) }
-  rescue ::Google::Apis::ClientError
+  rescue ::Google::Apis::ClientError => e
+    raise e unless e.status_code == 404
     Fog::Logger.warning("ignoring error in try_delete_instance")
   end
 end
