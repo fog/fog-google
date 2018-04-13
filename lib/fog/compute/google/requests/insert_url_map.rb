901b87377da648d331742d442e9792cd96bf8112
Artem <temikus@google.com>
2018-04-13 17:52:19 +1000

    Limit retries

--- lib/fog/compute/google/requests/insert_url_map.rb
+++ lib/fog/compute/google/requests/insert_url_map.rb
@@ -17,14 +17,16 @@ module Fog
             url_map.merge(:name => url_map_name)
           )
           # HACK - Currently URL map insert may fail even though the backend
-          # service operation is done.
+          # service operation is done. Retriable is not used to not add another
+          # runtime dependency.
           # TODO: Remove after that has been corrected.
           begin
+            retries ||= 0
             @compute.insert_url_map(@project, url_map_obj)
           rescue ::Google::Apis::ClientError
             Fog::Logger.warning("URL map insert failed, retrying...")
             sleep 10
-            retry
+            retry if (retries += 1) < 2
           end
         end
       end
