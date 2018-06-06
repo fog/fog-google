ac2ad90af88c3923ede1c9b0b1819796b4573959
Artem <temikus@google.com>
2018-06-06 19:14:39 +1000

    Add Model unit tests + light cleanup

---
+++ test/unit/compute/test_common_models.rb
@@ -0,0 +1,36 @@
+require "helpers/test_helper"
+
+class UnitTestModels < MiniTest::Test
+  def setup
+    Fog.mock!
+    @client = Fog::Compute.new(:provider => "Google", :google_project => "foo")
+
+    # Top-level ancestors we do not test
+    common_ancestors = [Fog::Model, Fog::Compute::Server]
+    # Do not test models that do not have a create method in API
+    exceptions = [ Fog::Compute::Google::MachineType,
+                   Fog::Compute::Google::Region,
+                   Fog::Compute::Google::DiskType,
+                   Fog::Compute::Google::Operation,
+                   Fog::Compute::Google::Zone,
+                   Fog::Compute::Google::Snapshot,
+                   Fog::Compute::Google::Project ]
+    # Enumerate all descendants of Fog::Model
+    descendants = ObjectSpace.each_object(Fog::Model.singleton_class).to_a
+
+    @models = descendants - common_ancestors - exceptions
+  end
+
+  def teardown
+    Fog.unmock!
+  end
+
+  def test_common_methods
+    # This tests whether Fog::Compute::Google models have common lifecycle methods
+    @models.each do |klass|
+      obj = klass.new
+      assert obj.respond_to?(:save), "#{klass} should have a .save method"
+      assert obj.respond_to?(:destroy), "#{klass} should have a .destroy method"
+    end
+  end
+end
