a7dd05a1f3a642ae95977f09684c4c4e794e2ecc
Artem <temikus@google.com>
2018-06-23 14:38:20 +1000

    Create failing test for #17

--- test/integration/compute/test_servers.rb
+++ test/integration/compute/test_servers.rb
@@ -15,6 +15,23 @@ class TestServers < FogIntegrationTest
     server.wait_for { ready? }
     server.set_metadata({ "foo" => "bar", "baz" => "foo" }, false)
     assert_equal [{ :key => "foo", :value => "bar" },
-                  { :key=>"baz", :value=>"foo" }], server.metadata[:items]
+                  { :key => "baz", :value => "foo" }], server.metadata[:items]
+  end
+
+  def test_bootstrap
+    key = "ssh-rsa IAMNOTAREALSSHKEYAMA== user@host.subdomain.example.com"
+    user = "username"
+
+    File.stub :read, key do
+      # Name is set this way so it will be cleaned up by CollectionFactory
+      server = @subject.bootstrap({ :name => "#{CollectionFactory::PREFIX}-#{Time.now.to_i}",
+                                    :username => user })
+      boot_disk = server.disks.find { |disk| disk[:boot] }
+
+      assert_equal(server.status, "RUNNING", "Bootstrapped server should be running")
+      assert_equal(server.public_key, key, "Bootstrapped server should have a public key set")
+      assert_equal(server.username, user, "Bootstrapped server should have a user set")
+      assert(boot_disk[:auto_delete], "Bootstrapped server should have disk set to autodelete")
+    end
   end
 end
