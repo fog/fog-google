cd4ace46af2ef03cf5682eb5dc75f64796d4c904
Artem <temikus@google.com>
2018-06-23 16:10:59 +1000

    Update example
    Some work towards #368

--- examples/bootstrap.rb
+++ examples/bootstrap.rb
@@ -1,18 +1,27 @@
 # All examples presume that you have a ~/.fog credentials file set up.
 # More info on it can be found here: http://fog.io/about/getting_started.html
+# Code can be ran by simply invoking `ruby bootstrap.rb`
+# Note: this example will require 'net-ssh' gem to be installed
 
 require "bundler"
 Bundler.require(:default, :development)
-# Uncomment this if you want to make real requests to GCE (you _will_ be billed!)
-# WebMock.disable!
 
-def test
-  connection = Fog::Compute.new(:provider => "Google")
+p "Connecting to google..."
+p "======================="
+connection = Fog::Compute.new(:provider => "Google")
 
-  server = connection.servers.bootstrap
+p "Bootstrapping a server..."
+p "========================="
+server = connection.servers.bootstrap
 
-  raise "Could not bootstrap sshable server." unless server.ssh("whoami")
-  raise "Could not delete server." unless server.destroy
-end
+p "Waiting for server to be sshable..."
+p "==================================="
+server.wait_for { sshable? }
 
-test
+p "Trying to send an SSH command..."
+p "================================"
+raise "Could not bootstrap sshable server." unless server.ssh("whoami")
+
+p "Deleting a server..."
+p "===================="
+raise "Could not delete server." unless server.destroy
