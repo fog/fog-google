f70dbd358d47ac59e3969aa80249dcf396268307
Artem <temikus@google.com>
2018-07-17 12:16:50 +1000

    Fix TargetPool get_health tests

--- test/integration/compute/loadbalancing/test_target_pools.rb
+++ test/integration/compute/loadbalancing/test_target_pools.rb
@@ -20,6 +20,16 @@ class TestTargetPools < FogIntegrationTest
     server = @servers.get(target_pool.instances[0].split("/").last)
     server.wait_for { ready? }
 
+    # XXX This is not partucularly elegant but if the instance doesn't respond
+    # to the healthcheck at all (for example there's nothing listening) it'll
+    # enter "No instance health info was found." state, during which the API
+    # will return an empty health object instead of UNHEALTHY.
+    # To avoid setting up a brittle environment with a live healthcheck we just
+    # stop the instance so health check returns UNHEALTHY and we can test that
+    # all fields are returned correctly.
+    server.stop
+    server.wait_for { stopped? }
+
     # There's no way to track the readiness of the instance resource in a target pool,
     # so wrapping in a soft retry:
     begin
@@ -30,9 +40,16 @@ class TestTargetPools < FogIntegrationTest
       retry if (retries += 1) < 3
     end
 
-    assert_equal(target_pool.get_health[server.self_link][0][:instance], server.self_link,
-                 "target_pool should return instance health details")
-    assert_equal(target_pool.get_health(server.name)[server.self_link][0][:instance], server.self_link,
+    refute_empty(target_pool.health_checks, "Target pool should have a health check")
+    assert_equal(2, target_pool.instances.count, "Target pool should have 2 instances")
+    assert_equal([{ :health_state => "UNHEALTHY",
+                    :instance => server.self_link }],
+                 target_pool.get_health[server.self_link],
+                 "Target pool should return a proper health check list")
+    assert_equal({ server.self_link =>
+                      [{ :health_state => "UNHEALTHY",
+                         :instance => server.self_link }] },
+                 target_pool.get_health(server.name),
                  "target_pool should return instance health details when an instance is specified")
   end
 end
