b54b19222a95524f21263d1619106c66fcd1bf34
Stan Hu <stanhu@gmail.com>
2018-08-18 14:09:37 -0700

    Add tests for Storage JSON requests

--- lib/fog/storage/google_json/mock.rb
+++ lib/fog/storage/google_json/mock.rb
@@ -5,8 +5,11 @@ module Fog
         include Utils
         include Fog::Google::Shared
 
+        MockClient = Struct.new(:issuer)
+
         def initialize(options = {})
           shared_initialize(options[:google_project], GOOGLE_STORAGE_JSON_API_VERSION, GOOGLE_STORAGE_JSON_BASE_URL)
+          @client = MockClient.new('test')
         end
 
         def signature(_params)
---
+++ test/unit/storage/test_json_requests.rb
@@ -0,0 +1,45 @@
+require "helpers/test_helper"
+
+class UnitTestJsonRequests < MiniTest::Test
+  def setup
+    Fog.mock!
+    @client = Fog::Storage.new(provider: "google",
+                               google_client_email: "",
+                               google_project: "",
+                               google_json_key_location: "")
+  end
+
+  def teardown
+    Fog.unmock!
+  end
+
+  def test_get_url_path_has_query_params
+    url = @client.get_object_url("bucket",
+                                 "just some file.json",
+                                 Time.now + 2 * 60,
+                                 query: { "projection" => 'full, noAcl"' })
+
+    assert_match(/projection=full%2C%20noAcl/, url,
+                 "query string should be escaped")
+  end
+
+  def test_get_url_filter_nil_query_params
+    url = @client.get_object_url("bucket",
+                                 "just some file.json",
+                                 Time.now + 2 * 60,
+                                 query: { "projection" => nil })
+
+    refute_match(/projection/, url,
+                 "nil query params should be omitted")
+  end
+
+  def test_put_url_path_is_properly_escaped
+    url = @client.put_object_url("bucket",
+                                 "just some file.json",
+                                 Time.now + 2 * 60,
+                                 "Content-Type" => "application/json")
+
+    assert_match(/just%20some%20file\.json/, url,
+                 "space should be escaped with '%20'")
+  end
+end
