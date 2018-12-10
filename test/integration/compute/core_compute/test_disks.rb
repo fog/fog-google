1279881a7af8dfc7f0f3a37e2ccdbf53cbb3f1dc
Artem Yakimenko <temikus@google.com>
2018-12-10 18:08:30 +1100

    Add tests for attached disk objects

--- test/integration/compute/core_compute/test_disks.rb
+++ test/integration/compute/core_compute/test_disks.rb
@@ -8,4 +8,25 @@ class TestDisks < FogIntegrationTest
     @subject = Fog::Compute[:google].disks
     @factory = DisksFactory.new(namespaced_name)
   end
+
+  def test_get_as_configs
+    disk = @factory.create
+    disk.wait_for { ready? }
+
+    example = disk.get_as_boot_disk
+    config = { :auto_delete=>false,
+               :boot=>true,
+               :source=>disk.self_link,
+               :mode=>"READ_WRITE",
+               :type=>"PERSISTENT" }
+    assert_equal(example, config)
+
+    example_with_params = disk.get_as_boot_disk(false, true)
+    config_with_params = { :auto_delete=>true,
+                           :boot=>true,
+                           :source=>disk.self_link,
+                           :mode=>"READ_ONLY",
+                           :type=>"PERSISTENT" }
+    assert_equal(example_with_params, config_with_params)
+  end
 end
